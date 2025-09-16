04869a0c4be6e5f3abd60fef733d6d2f4ef1d744
vweinzinger <131366922+vweinzinger@users.noreply.github.com>
2025-09-16 13:31:34 +0200

    Add files via upload

---
+++ Spirit-of-HIT_Website/src/main/java/tgm/ac/at/Main.java
@@ -0,0 +1,9 @@
+package tgm.ac.at;
+import org.springframework.boot.SpringApplication;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
